1dc88c50a292da689429bdd60592ca488fa33f7e
emmad <emmadenie@gmail.com>
2021-03-20 09:17:24 +0100

    specs

---
+++ specificaties.js
@@ -0,0 +1,38 @@
+Componenten:
+    Achtergrond
+button
+cursor
+
+Technologieen:
+    javascript
+html
+css
+
+Specificaties:
+    - javascript
+        [] score bijhouden onClick.
+    [] misklikken score naar null.
+    [] score > 100 door naar Level 2
+
+- dom bewerken
+    [] listener -> score veranderen in DOM
+    [] level veranderen in dom
+
+- html structuur / Dom
+    []achtergrondopbouw
+    []tonen wat je score is
+    []tonen welk level je zit
+    []Titel van het spel tonen
+
+- CSS / stylesheet
+    [] styling button
+    [] styling achtergrond + HTMLelements
+    [] hogere level is een kleinere button
+    [] button van kleur laten veranderen
+    [] bewegen van de button - css transitions
+
+- BONUS
+    [] kleur waar je veel punten voor krijgt een spastischere transition
+    [] bonus ronde - als je op de button klikt terwijl die goud is kom je in een bonus ronde die 10 seconden duurt.
+    In deze ronde kan je niet dood gaan, gaat het achtergrond knipperen en wordt er muziek afgespeeld.
+
